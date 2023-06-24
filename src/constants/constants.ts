2702a132d5c7b37405d69b88d754b0bfab32d753
Arturo Barrantes Vasquez <arturo.barrantesv@pucp.pe>
2023-06-24 17:17:09 -0500

    Revert "update constants"
    This reverts commit 44d834faf8fac9681ce4f568f6aeafcdddee8163.

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -7,3 +7,10 @@ export enum UserType {
 export const jwtConstants = {
   secret: 'gato',
 };
+
+export const twloConstants = {
+  twilioAuthToken: process.env.TWILIO_AUTH_TOKEN,
+  twilioAccountSID: process.env.TWILIO_ACCOUNT_SID,
+  twilioPhoneNumber: process.env.TWILIO_PHONE_NUMBER,
+  twilioSecret: process.env.TWLIO_SECRRET,
+};
