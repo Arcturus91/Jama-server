1c87a895f714bad048a95af75ae2720870a8abbd
Arturo Barrantes Vasquez <arturo.barrantesv@pucp.pe>
2023-06-24 19:49:46 -0500

    update nestjs config service

--- src/twilio/twilio.service.ts
+++ src/twilio/twilio.service.ts
@@ -1,21 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { Twilio } from 'twilio';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class TwilioWhatsappService {
   private twilioClient: Twilio;
   private twilioNumber: string;
   private jamaNumber: string;
-
-  constructor() {
-    const accountSid = process.env.TWILIO_ACCOUNT_SID;
-    const authToken = process.env.TWILIO_AUTH_TOKEN;
+  constructor(private configService: ConfigService) {
+    const accountSid = this.configService.get('TWILIO_ACCOUNT_SID');
+    const authToken = this.configService.get('TWILIO_AUTH_TOKEN');
     this.twilioClient = new Twilio(accountSid, authToken);
-    this.twilioNumber = process.env.TWILIO_SENDER_NUMBER;
-    this.jamaNumber = process.env.JAMA_NUMBER;
   }
 
   sendMessages(userId: string, mealName: string) {
+    this.twilioNumber = this.configService.get('TWILIO_SENDER_NUMBER');
+    this.jamaNumber = this.configService.get('JAMA_NUMBER');
+
     this.twilioClient.messages
       .create({
         body: `Ha llegado un pedido de userId: ${userId}. Solicitó un: ${mealName}`,
