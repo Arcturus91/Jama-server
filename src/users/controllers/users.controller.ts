91078d2402076a3a0b8617a98fdcdf422233769f
Arturo Barrantes Vasquez <arturo.barrantesv@pucp.pe>
2023-06-24 19:03:19 -0500

    Twilio service 100% implemented

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -111,6 +111,10 @@ export class UsersController {
     );
 
     await this.mealsService.updateMeal(mealId, +quantity);
+    this.twilioWhatsappService.sendMessages(
+      newOrder.user.id,
+      newOrder.meal.name,
+    );
     return newOrder;
     //session.orderId = newOrder.id;
     //!eventually, we will implement addMealToOrder(). But need to change Order entity relation with meals to Many to Many.
